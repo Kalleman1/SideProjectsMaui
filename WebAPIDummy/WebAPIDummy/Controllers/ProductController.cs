aff9a94a963d654941d18ab3e26f6a2b647171d3
Kalleman1 <kaper1@live.dk>
2023-03-20 10:09:51 +0100

    dummyapi

---
+++ WebAPIDummy/WebAPIDummy/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIDummy.Models;
+
+namespace WebAPIDummy.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var products = new List<Product>
+            {
+                new Product { Name = "Product 1", Description = "Description 1", Price = 10.0, Amount = 100 },
+                new Product { Name = "Product 2", Description = "Description 2", Price = 20.0, Amount = 200 },
+                new Product { Name = "Product 3", Description = "Description 3", Price = 30.0, Amount = 300 }
+            };
+
+            return products;
+        }
+    }
+}
+
---
+++ WebAPIDummy/WebAPIDummy/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPIDummy.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+    }
+}
